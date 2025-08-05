42c8a413d7b5569f0c1c98c229df269c08a1101a
StupidHub <110139840+MyStupidHubs@users.noreply.github.com>
2025-08-05 18:18:42 -0300

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "5.39.63.207",
-      port:  8262,
+      name: "andre_andrezinh2",
+      password: "DejLqWEFBIlzi7Z2",
+      host: "eu-ro-01.wisp.uno",
+      port:  9969,
       secure: false
     }
   ]
