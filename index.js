8b0adf1b0f503cf98cbf20c5d2ec128d6d27003e
StupidHub <110139840+MyStupidHubs@users.noreply.github.com>
2026-08-28 13:27:23 -0300

    Mark non-blocking now-playing panel recovery v7

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log(`[ BOOT ] DforDog Riffy websocket-recovery v6 loaded | commit=${process.env.RENDER_GIT_COMMIT || process.env.GIT_COMMIT || 'unknown'}`);
+console.log(`[ BOOT ] DforDog Riffy websocket-recovery v7 loaded | commit=${process.env.RENDER_GIT_COMMIT || process.env.GIT_COMMIT || 'unknown'}`);
 require("./utils/riffyVoiceLifecycleFix.js");
 require("./utils/riffyRestEventFallback.js");
 require("./bot.js");
