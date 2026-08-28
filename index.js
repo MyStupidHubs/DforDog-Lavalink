d4f087a140452c69ab2a11bd27b1dc0182412c72
StupidHub <110139840+MyStupidHubs@users.noreply.github.com>
2026-08-28 13:40:34 -0300

    Load v8 full trackStart panel recovery

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-console.log(`[ BOOT ] DforDog Riffy websocket-recovery v7 loaded | commit=${process.env.RENDER_GIT_COMMIT || process.env.GIT_COMMIT || 'unknown'}`);
+console.log(`[ BOOT ] DforDog Riffy websocket-recovery v8 loaded | commit=${process.env.RENDER_GIT_COMMIT || process.env.GIT_COMMIT || 'unknown'}`);
 require("./utils/riffyVoiceLifecycleFix.js");
 require("./utils/riffyRestEventFallback.js");
+require("./utils/riffyTrackStartPanelRecovery.js");
 require("./bot.js");
