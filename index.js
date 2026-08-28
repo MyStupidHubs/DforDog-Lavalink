2066e914b05332bcd64a5c27175f59c1918c6263
StupidHub <110139840+MyStupidHubs@users.noreply.github.com>
2026-08-28 11:34:45 -0300

    Add startup marker for Riffy recovery deploy

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
+console.log(`[ BOOT ] DforDog Riffy event-recovery v4 loaded | commit=${process.env.RENDER_GIT_COMMIT || process.env.GIT_COMMIT || 'unknown'}`);
 require("./utils/riffyVoiceLifecycleFix.js");
 require("./bot.js");
-
