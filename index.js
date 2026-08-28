8100c3f60c809cfd9c0dc78874824d13e841ba5f
StupidHub <110139840+MyStupidHubs@users.noreply.github.com>
2026-08-28 17:49:38 -0300

    Load v9 websocket heartbeat recovery before music event fallbacks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-console.log(`[ BOOT ] DforDog Riffy websocket-recovery v8 loaded | commit=${process.env.RENDER_GIT_COMMIT || process.env.GIT_COMMIT || 'unknown'}`);
+console.log(`[ BOOT ] DforDog Riffy websocket-recovery v9 loaded | commit=${process.env.RENDER_GIT_COMMIT || process.env.GIT_COMMIT || 'unknown'}`);
 require("./utils/riffyVoiceLifecycleFix.js");
+require("./utils/riffyWsHeartbeatRecovery.js");
 require("./utils/riffyRestEventFallback.js");
 require("./utils/riffyTrackStartPanelRecovery.js");
 require("./bot.js");
